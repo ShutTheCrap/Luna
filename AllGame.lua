c82fc9e0ca2c2a12e6b16a3b339e64b929686111
Mene <101266283+ShutTheCrap@users.noreply.github.com>
2022-04-02 16:57:49 +0700

    Create AllGame.lua

---
+++ AllGame.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShutTheCrap/Luna/main/BF.lua"))()
+elseif game.PlaceId == 3198259055 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShutTheCrap/Luna/main/SS.lua"))()
+else
+game:Shutdown()
+end
